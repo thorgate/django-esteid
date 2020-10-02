5d47a18674b527d66c4d9c7bfb988bd8303eee04
Yuriy Shatrov <yks-uno@ya.ru>
2020-10-02 10:32:11 +0300

    Host-only ID Card signing
    Enable signing with ID card without any external services
    * update actions to use the esteid.bdoc2 package for host-based signature generation
    * update bundled Esteid.main.js script
    * add utils for session handling and getting signature info in a DDS compatible format

--- esteid/actions.py
+++ esteid/actions.py
@@ -1,11 +1,28 @@
-import binascii
+import os
+from tempfile import NamedTemporaryFile
 
-from .digidocservice.service import DigiDocError, DigiDocNotOk
+import binascii
+import logging
+import typing
+
+from .bdoc2.container import BDoc2File
+from .bdoc2.exceptions import BDoc2Error
+from .bdoc2.signature_verifier import verify_cryptography
+from .bdoc2.utils import prepare_signature, finalize_signature
+from .bdoc2.xmlsig import XmlSignature
+from .digidocservice.service import DigiDocError
+from .session import open_container, get_esteid_session, update_esteid_session, delete_esteid_session
+
+if typing.TYPE_CHECKING:
+    from .generic import GenericDigitalSignViewMixin
+
+
+logger = logging.getLogger(__name__)
 
 
 class BaseAction(object):
     @classmethod
-    def do_action(cls, view, action_kwargs):
+    def do_action(cls, view: "GenericDigitalSignViewMixin", action_kwargs):
         raise NotImplementedError
 
 
@@ -17,69 +34,142 @@ class NoAction(object):
 
 class IdCardPrepareAction(BaseAction):
     @classmethod
-    def do_action(cls, view, action_kwargs):
-        service = view.get_service()
+    def do_action(cls, view: "GenericDigitalSignViewMixin", action_kwargs: dict = None, *, certificate: str = None,
+                  container_path: str = None):
+        """
+        The old API is to pass a dict, the keyword args are added here for clarity as to what the method accepts
 
-        if not view.get_files():
+        :param view:
+        :param action_kwargs:
+        :param certificate: HEX-encoded certificate from the ID card
+        :return:
+        """
+        request = view.request
+
+        if not certificate:
+            certificate = action_kwargs['certificate']
+
+        if container_path is None:
+            container_path = action_kwargs.get("container_path", "")
+
+        delete_esteid_session(request)
+
+        if not certificate:
+            return {
+                'success': False,
+                'code': 'BAD_CERTIFICATE',
+            }
+
+        certificate = binascii.a2b_hex(certificate)
+
+        files = view.get_files()
+
+        if not files:
             return {
                 'success': False,
                 'code': 'MIN_1_FILE',
             }
 
-        # Create signed document
-        service.create_signed_document()
+        container = open_container(container_path, files)
+        xml_sig = prepare_signature(certificate, container)
 
-        # add all files
-        for file in view.get_files():
-            service.add_datafile(file.file_name, file.mimetype, service.HASHCODE, file.size, file.content)
+        # save intermediate signature XML to temp file
+        with NamedTemporaryFile(delete=False) as f:
+            f.write(xml_sig.dump())
 
-        # Call prepare_signature
-        try:
-            response = service.prepare_signature(**action_kwargs)
-            response['success'] = True
-            return response
+        if not container.name:
+            with NamedTemporaryFile(delete=False) as temp_container_file:
+                container.save(temp_container_file.name)
+        else:
+            temp_container_file = None
 
-        except DigiDocNotOk as e:
-            return {
-                'success': False,
-                'error_code': e.code,
-                'message': str(e),
-            }
+        signed_digest = xml_sig.digest()
+        digest_hash_b64 = binascii.b2a_base64(signed_digest).decode()
 
-        except DigiDocError as e:
-            return {
-                'success': False,
-                'error_code': e.error_code,
-                'message': service.ERROR_CODES.get(int(e.error_code), service.ERROR_CODES[100])
-            }
+        update_esteid_session(
+            request,
+            signed_hash=digest_hash_b64,  # probably not needed, as we can take the hash from the signature XML
+            temp_signature_file=f.name,
+            temp_container_file=temp_container_file.name if temp_container_file else None,
+        )
+
+        logger.debug("Signature hash base64: %s", digest_hash_b64)
+
+        return {
+            'success': True,
+            'digest': binascii.b2a_hex(signed_digest).decode(),
+        }
 
 
 class IdCardFinishAction(BaseAction):
     @classmethod
-    def do_action(cls, view, action_kwargs):
-        service = view.get_service()
+    def do_action(cls, view: "GenericDigitalSignViewMixin", action_kwargs: dict = None, *, signature_value: str = None,
+                  container_path: str = None):
+        """
+        The old API is to pass a dict, the keyword args are added here for clarity as to what the method accepts
+
+        :param view:
+        :param action_kwargs:
+        :param signature_value: a HEX encoded signature, as received from `hwcrypto.js`
+        :return:
+        """
+        request = view.request
+        session_data = get_esteid_session(request)
+        if not session_data:
+            return {
+                'success': False,
+                'code': 'NO_SESSION',
+            }
+
+        if signature_value is None:
+            signature_value = action_kwargs['signature_value']
+            if not signature_value:
+                return {
+                    'success': False,
+                    'code': 'BAD_SIGNATURE',
+                }
+
+        if container_path is None:
+            container_path = action_kwargs.get("container_path", "")
+
+        logger.debug("Signature HEX: %s", signature_value)
+
+        signed_hash_b64 = session_data['signed_hash']
+        signature_value = binascii.a2b_hex(signature_value)
+
+        temp_signature_file = session_data['temp_signature_file']
+        temp_container_file = session_data['temp_container_file']
+
+        with open(temp_signature_file, 'rb') as f:
+            xml_sig = XmlSignature(f.read())
+        os.remove(temp_signature_file)
+
+        verify_cryptography(xml_sig.get_certificate_value(), signature_value, binascii.a2b_base64(signed_hash_b64), prehashed=True)
+
+        if temp_container_file:
+            # Load a partially prepared BDoc from a tempfile and clean it up
+            container = BDoc2File(temp_container_file)
+            os.remove(temp_container_file)
+        else:
+            container = BDoc2File(container_path)
+
+        xml_sig.add_signature_value(signature_value)
 
         try:
-            # Should we also pass back the signed_doc_info (?)
-            service.finalize_signature(**action_kwargs)
-
-            return {
-                'success': True,
-            }
-
-        except DigiDocNotOk as e:
+            finalize_signature(xml_sig)
+        except BDoc2Error:
+            logger.exception("Signature confirmation service error")
             return {
                 'success': False,
-                'error_code': e.code,
-                'message': str(e),
+                'code': 'SERVICE_ERROR',
             }
 
-        except DigiDocError as e:
-            return {
-                'success': False,
-                'error_code': e.error_code,
-                'message': service.ERROR_CODES.get(int(e.error_code), service.ERROR_CODES[100])
-            }
+        container.add_signature(xml_sig)
+
+        return {
+            'success': True,
+            'container': container,
+        }
 
 
 class SignCompleteAction(BaseAction):
---
+++ esteid/session.py
@@ -0,0 +1,62 @@
+import os
+from typing import List
+
+from esteid.bdoc2.container import BDoc2File
+from esteid.bdoc2.exceptions import NoFilesToSign
+from esteid.digidocservice.service import DataFile
+
+
+SESSION_KEY = '__esteid_session'
+
+
+class EsteidSessionError(Exception):
+    pass
+
+
+def get_esteid_session(request):
+    return request.session.get(SESSION_KEY, {})
+
+
+def start_esteid_session(request):
+    delete_esteid_session(request)
+    return {}
+
+
+def update_esteid_session(request, **kwargs):
+    session = request.session
+    data = session.get(SESSION_KEY, {})
+    data.update(kwargs)
+    session[SESSION_KEY] = data  # Django session object requires setting keys explicitly
+
+
+def delete_esteid_session(request):
+    session_data = request.session.pop(SESSION_KEY, {})
+
+    temp_signature_file = session_data.get('temp_signature_file')
+    if temp_signature_file:
+        try:
+            os.unlink(temp_signature_file)
+        except OSError:
+            pass
+
+    temp_container_file = session_data.get('temp_container_file')
+    if temp_container_file:
+        try:
+            os.unlink(temp_container_file)
+        except OSError:
+            pass
+
+
+def open_container(container_path: str = None, files_to_sign: List[DataFile] = None):
+    """Open an existing BDoc container, or create a container with files to sign"""
+    if container_path:
+        # Take files from an existing container, ignore files_to_sign
+        container = BDoc2File(container_path)
+    elif files_to_sign:
+        container = BDoc2File()
+        for data_file in files_to_sign:
+            container.add_file(data_file.file_name, data_file.content, data_file.mimetype)
+    else:
+        raise NoFilesToSign('No files to sign')  # this is a 500 error because it's not the user's fault
+
+    return container
