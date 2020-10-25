6d5e88bfd28bd13d2636c29947dd2c317c9b9e62
Yuriy Shatrov <yks-uno@yandex.ru>
2020-10-25 19:22:30 +0200

    small improvement for test urls

--- esteid/urls.py
+++ esteid/urls.py
@@ -22,15 +22,14 @@ urlpatterns = []
 if settings.DEBUG:
     urlpatterns += [
         path("", SKTestView.as_view(), name="sk_test"),
+        path("download/", TestDownloadContainerView.as_view(), name="download_signed_container"),
+
         url(r"^id/start/", TestIdCardSignView.as_view(), name="test_id_start"),
         url(r"^id/finish/", TestIdCardFinishView.as_view(), name="test_id_finish"),
-        url(r"^id/done/", TestDownloadContainerView.as_view(), name="test_id_finalize"),
         url(r"^mid/start/", TestMobileIdSignView.as_view(), name="test_mid_start"),
         url(r"^mid/status/", TestMobileIdStatusView.as_view(), name="test_mid_status"),
-        url(r"^mid/done/", TestDownloadContainerView.as_view(), name="test_mid_finalize"),
         url(r"^smartid/start/", TestSmartIdSignView.as_view(), name="test_smartid_start"),
         url(r"^smartid/status/", TestSmartIdStatusView.as_view(), name="test_smartid_status"),
-        url(r"^smartid/done/", TestDownloadContainerView.as_view(), name="test_smartid_finalize"),
     ]
 
     urlpatterns += static(settings.STATIC_URL, document_root=os.path.dirname(__file__) + "/static/")
